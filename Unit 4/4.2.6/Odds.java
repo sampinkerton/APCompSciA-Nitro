e6d018fc82fce224604609262b7f78ae65b12c1e
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 10:14:28 -0600

    Create Odds.java

---
+++ Unit 4/4.2.6/Odds.java
@@ -0,0 +1,13 @@
+public class Odds
+{
+    public static void main(String[] args)
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            if(i % 2 == 1)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
