51f4d1cc60298a1954d8d9017b75e297aa7b277d
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 01:17:38 -0600

    Create Pizza.java

---
+++ Unit 2/2.1.8/Pizza.java
@@ -0,0 +1,8 @@
+public class Pizza
+{
+    // Add your instance variables here
+    private boolean pepperoni;
+    private boolean cheese;
+    private double temperature;
+    private boolean stuffedcrust;
+}
