42b4ae64f8c94d0d207bc9551c49477794444cf9
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 01:06:10 -0600

    Create MyAge.java

---
+++ Unit 1/1.5.6/MyAge.java
@@ -0,0 +1,12 @@
+public class MyAge
+{
+    public static void main(String[] args)
+    {
+        int age = 27;
+        System.out.println("Current Age is: " + age);
+        age++;
+        System.out.println("Age in one year is: " + age);
+        age--;
+        System.out.println("Current Age is once again: " + age);
+    }
+}
