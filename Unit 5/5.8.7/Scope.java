a54da7c9912ada37ed340797c6c4a9f673d8a7a0
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 12:55:31 -0600

    Create Scope.java

---
+++ Unit 5/5.8.7/Scope.java
@@ -0,0 +1,39 @@
+//No tester for this one. -MP
+public class Scope
+{
+    private int a;
+    private int b;
+    private int c;
+
+    public Scope(){
+        a = 5;
+        b = 10;
+        c = 15;
+    }
+
+    public void printScope(){
+       System.out.println(getA)
+    }
+
+	public int getA() {
+		return a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public int getC() {
+		return c;
+	}
+
+    public int getD(){
+        int d = a + c;
+        return d;
+    }
+
+	public int getE() {
+        int e = b + c;
+		return e;
+	}
+}
