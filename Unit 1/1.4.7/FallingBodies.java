f5aff3bfea507a66c371120b33603f1a09875c67
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 01:04:44 -0600

    Create FallingBodies.java

---
+++ Unit 1/1.4.7/FallingBodies.java
@@ -0,0 +1,12 @@
+public class FallingBodies
+{
+    public static void main(String[] args)
+    {
+        double g = 9.8;
+        double t = 10;
+        
+        
+        System.out.println("You're height is " + (.5*g*t*t));
+        System.out.println("The velocity is "+ (g*t));
+    }
+}
