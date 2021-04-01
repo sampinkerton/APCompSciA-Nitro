9c2ee3007033c31540fefa4d2726e576ec79031a
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 00:59:51 -0600

    Create Timeline.java

---
+++ Unit 1/1.2.9/Timeline.java
@@ -0,0 +1,32 @@
+public class Timeline
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Age     | M/YY - Description");
+        System.out.println("6       |");
+        System.out.println("        |");
+        System.out.println("        | 8/10 - probably started school");
+        System.out.println("        |");
+        System.out.println("8       |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("9       | 4/13 - started playing minecraft");
+        System.out.println("10      | 11/13 - turned a decade old");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("12      |");
+        System.out.println("        |");
+        System.out.println("        | 8/15 - probably started middle school");
+        System.out.println("        |");
+        System.out.println("14      |");
+        System.out.println("        |");
+        System.out.println("        | 8/18 - probably started high school");
+        System.out.println("        | 1/20 - 2020 started");
+        System.out.println("16      |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("        |");
+        System.out.println("18      |");
+    }
+}
