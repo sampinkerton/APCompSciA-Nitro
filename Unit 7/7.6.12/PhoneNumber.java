23df22e9a41d80dc12c21e874ea7a77c311a5547
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 13:40:27 -0600

    Create PhoneNumber.java

---
+++ Unit 7/7.6.12/PhoneNumber.java
@@ -0,0 +1,25 @@
+public class PhoneNumber
+{
+    private String name;
+    private String number;
+
+    public PhoneNumber(String name, String number)
+    {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getNumber()
+    {
+        return number;
+    }
+    public String toString()
+    {
+        return name + " " + number;
+    }
+}
