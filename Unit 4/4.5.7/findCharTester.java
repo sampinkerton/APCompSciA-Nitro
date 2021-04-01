bec9118f39f7d18e580f9ed7753d26c4d5c3e721
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 10:22:23 -0600

    Create findCharTester.java

---
+++ Unit 4/4.5.7/findCharTester.java
@@ -0,0 +1,21 @@
+public class findCharTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        String word = "This is a sentence";
+        String key = "a";
+        System.out.println("The character "+ key+ " is in the String "+ word + ": " + findChar(word, key));
+    }
+    
+    public static boolean findChar(String string, String key)
+    {
+        // Write a more efficient version of isChar than the one in the exercise description
+        // Use the hint!
+        if (string.indexOf(key) < 0 )
+        {
+            return false;
+        }
+        return tru
+    }
+}
