6a60b45672c186c75c4ce9b5aa71c80f30d446f5
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 10:27:31 -0600

    Create DogTester.java

---
+++ Unit 5/5.2.6/DogTester.java
@@ -0,0 +1,11 @@
+public class DogTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Dog karel = new Dog("Karel", 7, 15.6);
+        Dog clover = new Dog("Clover", 7);
+        System.out.print(karel);
+        System.out.print(clover);
+    }
+}
