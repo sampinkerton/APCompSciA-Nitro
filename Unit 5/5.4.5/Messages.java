5b379caa25873927eaad233b1bc3c270787bcd37
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 10:32:30 -0600

    Create Messages.java

---
+++ Unit 5/5.4.5/Messages.java
@@ -0,0 +1,12 @@
+public class Messages
+{
+    public static void main(String[] args)
+    {
+        // Your code here.
+        // Create two TextMessage objects and print them out.
+        TextMessage one = new TextMessage("man One","Man Two","M E S S A G E");
+        TextMessage two = new TextMessage("man Two","Man One","M E S S A G E R E C I E V E D");
+        System.out.println(one);
+        
+    }
+}
