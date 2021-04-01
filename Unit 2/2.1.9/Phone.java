99ea7dd1dcf26143d83feb6f579a51a5195351f7
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 01:18:32 -0600

    Create Phone.java

---
+++ Unit 2/2.1.9/Phone.java
@@ -0,0 +1,23 @@
+
+/**
+ * 
+ * The phone class represents a cellular phone.
+ * 
+ * Add the instance variables you think a phone would need
+ */ 
+
+public class Phone
+{
+
+    // Attributes
+   private double length;
+   private double height;
+   private double depth;
+   
+   private String manufacterer;
+   private double storage;
+   private double model;
+   
+    
+
+}
