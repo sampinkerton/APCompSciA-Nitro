fdc4a6d4b49857272c43704992b30953246d44b8
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 10:05:53 -0600

    Create Divisibility.java

---
+++ Unit 3/3.5.8/Divisibility.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Divisibility 
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the dividend: ");
+        int dividend = input.nextInt();
+        System.out.print("Enter the divisor: ");
+        int divisor = input.nextInt();
+        
+        if(divisor == 0 || !(dividend % divisor == 0))
+        {
+            System.out.println(dividend + " is not divisible by " + divisor + "!");
+        }
+        else
+        {
+            System.out.println(dividend + " is divisible by " + divisor + "!");
+        }
+    }
+}
