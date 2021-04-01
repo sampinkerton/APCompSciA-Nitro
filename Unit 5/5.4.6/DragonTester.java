f7187bfd6c07d596f24289852707d07851161222
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 10:33:34 -0600

    Create DragonTester.java

---
+++ Unit 5/5.4.6/DragonTester.java
@@ -0,0 +1,9 @@
+public class DragonTester
+{
+    public static void main(String[] args)
+    {
+        // Create a Dragon here to test out the Dragon class!
+        Dragon shrekdragon = new Dragon(6,"fire");
+        System.out.println(shrekdragon.fight());
+    }
+}
