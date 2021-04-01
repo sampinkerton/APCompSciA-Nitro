4830ae91173c1b68bcc533a6c2a39d42faaae983
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 01:03:31 -0600

    Create Pyramid.java

---
+++ Unit 1/1.4.6/Pyramid.java
@@ -0,0 +1,9 @@
+public class Pyramid 
+{
+    public static void main(String[] args)
+    {
+        int numBlocks = 2500000;
+        double blockWeight = 2.5;
+        System.out.println("The pyramid weighs " + (numBlocks*blockWeight) + " tons" );
+    }
+}
