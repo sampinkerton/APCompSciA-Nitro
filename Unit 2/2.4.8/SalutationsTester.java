a723f6cc4a2c98af45ad10743d5ad52cb674333a
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 08:11:41 -0600

    Create SalutationsTester.java

---
+++ Unit 2/2.4.8/SalutationsTester.java
@@ -0,0 +1,9 @@
+public class SalutationsTester
+{
+    public static void main(String[] args)
+    {
+        Salutations romeo = new Salutations("Romeo");
+        romeo.addressLetter();
+        romeo.signLetter();
+    }
+}
