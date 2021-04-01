e68871565fe4406f1b53cdfe3771fd7318a08e5e
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 01:21:46 -0600

    Create Pizza.java

---
+++ Unit 2/2.2.9/Pizza.java
@@ -0,0 +1,25 @@
+public class Pizza
+{
+    // Add the instance variables here
+    private int size;
+    private String type;
+    private String toppings;
+    
+    
+    // Put the constructor here
+    public Pizza(String theType, String theToppings, int theSize)
+    {
+        
+        type = theType;
+        toppings = theToppings;
+        size = theSize;
+        
+    }
+    
+    // You don't need to do anything with this method
+    // Used to print the object
+    public String toString()
+    {
+        return size + " inch " + type + " pizza with " + toppings;
+    }
+}
