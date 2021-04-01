d7c82fb3a6286db02d017ea37bf92fbfbaf11989
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 01:00:47 -0600

    Create Variables.java

---
+++ Unit 1/1.3.5/Variables.java
@@ -0,0 +1,8 @@
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        int currentYear = 2020;
+        System.out.println("The current year is " + currentYear);
+    }
+}
