d04c07380b22d4e3803d4b55f6e6957616a4dff4
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 08:41:58 -0600

    Create Filer.java

---
+++ Unit 2/2.8.8/Filer.java
@@ -0,0 +1,60 @@
+public class Filer
+{
+    private String word;
+    
+    public Filer(String theWord)
+    {
+        word = theWord;
+    }
+    
+    // Returns the word
+    public String getWord()
+    {
+        return word;
+    }
+    
+    // Returns true if word comes
+    // before otherWord.
+    // Returns false otherwise.
+    public boolean comesBefore(String otherWord)
+    {
+        if(word.compareTo(otherWord) < 0)
+        {
+            return true;
+        }else
+        {
+            return false;   
+        }
+    }
+    
+    
+    // Returns true if word comes
+    // after otherWord.
+    // Returns false otherwise.
+    public boolean comesAfter(String otherWord)
+    {
+        if(word.compareTo(otherWord) > 0)
+        {
+            return true;
+        }else
+        {
+            return false;   
+        }
+    }
+    
+    
+    // Returns true if word is equal to otherWord.
+    // Returns false otherwise.
+    public boolean isEqual(String otherWord)
+    {
+        if(word.compareTo(otherWord) == 0)
+        {
+            return true;
+        }else
+        {
+            return false;   
+        }
+    }
+    
+    
+}
