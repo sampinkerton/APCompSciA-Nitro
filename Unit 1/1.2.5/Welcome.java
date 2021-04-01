59dee1fcb9c8c53e2bcf5b93b24bc76cdc0e839f
Arkus Corztuan <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-01 00:52:44 -0600

    Create Welcome.java

---
+++ Unit 1/1.2.5/Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My name is a pseudonym");
+        System.out.println("I like batman");
+    } 
+}
