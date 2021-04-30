a03a948000db8fe88975cf4f85dd98a162d03407
Sam Pinkerton <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-30 13:38:07 -0600

    Create EmployeeTester.java

---
+++ Unit 9/9.4.8/EmployeeTester.java
@@ -0,0 +1,11 @@
+public class EmployeeTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        System.out.println("\nm\n");
+       //it says uwu if you turn it upside down
+    }
+}
+
+//Grader needs 2 lines -MP
