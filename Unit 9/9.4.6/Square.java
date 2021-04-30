1969a5ac18f986db44fb55d4f18f4f3f463e2b81
Sam Pinkerton <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-30 10:53:24 -0600

    Create Square.java

---
+++ Unit 9/9.4.6/Square.java
@@ -0,0 +1,28 @@
+public class Square extends Rectangle {
+
+    // Call to the Rectangle constructor
+    public Square(double sideLength){
+       super(sideLength, sideLength);
+    }
+
+    // Return either the width or height from the superclass
+    public double getSideLength(){
+        return super.getWidth();
+    }
+
+    //Set both the width and height in the superclass
+    public void setSideLength(double sideLength){
+        super.setWidth(sideLength);
+        super.setHeight(sideLength);
+    }
+    
+    // Get the width and/or the height from the superclass
+    public double area(){
+      return (super.getHeight()*super.getWidth());
+    }
+
+    // Override to read: Square with side lengths ______
+    public String toString(){
+        return "" + "Square with side lengths " + super.getHeight();
+    }
+}
