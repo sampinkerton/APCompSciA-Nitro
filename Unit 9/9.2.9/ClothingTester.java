b2c12b166118a5047f2d424a216b4824db22ab10
Sam Pinkerton <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-29 23:33:10 -0600

    Create ClothingTester.java

---
+++ Unit 9/9.2.9/ClothingTester.java
@@ -0,0 +1,16 @@
+public class ClothingTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        TShirt a = new TShirt("L", "red", "denim");
+        Jeans b = new Jeans("S");
+        Sweatshirt c = new Sweatshirt("L", "green", true);
+        Jeans d = new Jeans("L");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
+//For some reason, this one needs you to actually make 4 things and "test" them. I did it by making 4 objects and printing them all out. Thats why this tester is included. -MP
