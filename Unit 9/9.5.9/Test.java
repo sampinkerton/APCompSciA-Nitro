70b1c56fd3b50683ad66d793318b471674b0b1f7
Sam Pinkerton <48970620+manifold-pinkerton@users.noreply.github.com>
2021-04-30 21:02:48 -0600

    Create Test.java

---
+++ Unit 9/9.5.9/Test.java
@@ -0,0 +1,19 @@
+public class Test extends Assignment {
+    
+    private String testDate;
+    
+    public Test(String name, double availablePoints, double earnedPoints, String testDate)
+    {
+        super(name, availablePoints, earnedPoints);
+        this.testDate = testDate;
+    }
+    
+    public String getTestDate()
+    {
+        return testDate;
+    }
+    public void setTestDate(String n)
+    {
+        testDate = n;
+    }
+}
